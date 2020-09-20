a8647002663329cac73fdfc2b68c03f42985301b
DaleNaci <dale.nash01@gmail.com>
2020-09-20 04:56:07 -0400

    Camera works w/ flip

--- react-native-app/screens/ARScreen.js
+++ react-native-app/screens/ARScreen.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default class ARScreen extends Component {
     render() {
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Camera!</Text>
+                <Text>AR!</Text>
             </View>
         );
     }
--- react-native-app/screens/CameraScreen.js
+++ react-native-app/screens/CameraScreen.js
@@ -1,11 +1,54 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component, useState } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
 
 export default class CameraScreen extends Component {
+
+    state = {
+        type: Camera.Constants.Type.back
+    };
+
+    async componentDidMount() {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({ hasCameraPermission: status === 'granted' });
+    }
+
     render() {
+        
+        if (this.state.hasPermission === null) {
+            return <View />;
+        }
+        if (this.state.hasPermission === false) {
+            return <Text>No access to camera</Text>;
+        }
+
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Camera!</Text>
+            <View style={{ flex: 1 }}>
+                <Camera style={{ flex: 1 }} type={this.state.type}>
+                    <View
+                        style={{
+                            flex: 1,
+                            backgroundColor: 'transparent',
+                            flexDirection: 'row',
+                        }}>
+                        <TouchableOpacity
+                            style={{
+                                flex: 0.1,
+                                alignSelf: 'flex-end',
+                                alignItems: 'center',
+                            }}
+                            onPress={() => {
+                                this.setState({
+                                    type: this.state.type === Camera.Constants.Type.back
+                                    ? Camera.Constants.Type.front
+                                    : Camera.Constants.Type.back
+                                });
+                            }}>
+                            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+                        </TouchableOpacity>
+                    </View>
+                </Camera>
             </View>
         );
     }
