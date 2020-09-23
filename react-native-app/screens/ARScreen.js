3aeb725ac7bc226d6bcce2baf8d0bc36ffc46e4d
DaleNaci <dale.nash01@gmail.com>
2020-09-23 16:44:06 -0400

    We gave up right about here

--- react-native-app/screens/ARScreen.js
+++ react-native-app/screens/ARScreen.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { ViroARScene, Viro3DObject } from 'react-viro';
+
 
 export default class ARScreen extends Component {
     render() {
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>AR!</Text>
+                <ViroARScene>
+                    <Viro3DObject source={{uri:"http://8c13f1872e6e.ngrok.io/get_file/test.obj"}}
+                        position={[-0.0, -5.5, -1.15]}
+                        materials={["heart"]}
+                        type="OBJ" />
+                </ViroARScene>
             </View>
         );
     }
